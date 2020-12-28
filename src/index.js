c60c30271ba4550eb50ff281d181b1931f2787c0
listarmans <74196413+listarmans@users.noreply.github.com>
2020-12-28 14:34:24 +0100

    forecast implemented

--- src/index.js
+++ src/index.js
@@ -54,19 +54,19 @@ function formatTime(time) {
 let time = document.querySelector("#time");
 time.innerHTML = formatTime(now);
 
-////////
 function showTemperature(response) {
+  celsiusTemperature = response.data.main.temp;
+  feelingTemperature = response.data.main.feels_like;
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = `${Math.round(
-    response.data.main.temp
+    celsiusTemperature
   )} °C`;
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
   document.querySelector("#feeling").innerHTML = `Feels like ${Math.round(
-    response.data.main.feels_like
-  )} °C`;
+    feelingTemperature)} °C`;
   document.querySelector("#humidity").innerHTML = `${Math.round(
     response.data.main.humidity
   )}%`;
@@ -75,10 +75,30 @@ function showTemperature(response) {
   )} km/h`;
 }
 
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = 
+  `<div class="col-2">
+                <div class="future-time">
+                 12:00
+                </div>
+                <img
+                 src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
+                 alt=""
+                />
+                <div class="weather-forecast-temperature">
+                 <strong>16</strong> 15
+                </div>
+            </div>`
+}
+
 function searchCity(city) {
   let apiKey = "06ca6b2e6fb5a01a965d59c5f70dc4cc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleSubmit(event) {
@@ -106,21 +126,26 @@ let currentLocalization = document.querySelector("#my-location");
 currentLocalization.addEventListener("click", localWeather);
 searchCity("Amsterdam");
 
-function toFahrenheit() {
-  let celcius = 14;
-  let fahrenheit = (celcius * 9) / 5 + 32;
-  celcius = fahrenheit;
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = `${celcius}°F`;
-}
-let currentTemperature = document.querySelector("#fahrenheit");
-currentTemperature.addEventListener("click", toFahrenheit);
-
-function toCelcius() {
-  let celcius = 14;
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = `${celcius}°C`;
+function showFahrenheitTemperature(event){
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  document.querySelector("#temperature").innerHTML = `${Math.round(fahrenheitTemperature)} °F`;
+  let fahrenheitFeelingTemperature = (feelingTemperature * 9) / 5 + 32;
+  document.querySelector("#feeling").innerHTML = `Feels like ${Math.round(fahrenheitFeelingTemperature)} °F`;
 }
 
-let backToCelcius = document.querySelector("#celcius");
-backToCelcius.addEventListener("click", toCelcius);
+function showCelsiusTemperature(event){
+   document.querySelector("#temperature").innerHTML = `${Math.round(
+    celsiusTemperature
+  )} °C`;
+  document.querySelector("#feeling").innerHTML = `Feels like ${Math.round(
+    feelingTemperature)} °C`;
+}
+
+let celsiusTemperature = null;
+let feelingTemperature = null;
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", showFahrenheitTemperature);
+
+let celsius = document.querySelector("#celsius");
+celcius.addEventListener("click", showCelsiusTemperature);
